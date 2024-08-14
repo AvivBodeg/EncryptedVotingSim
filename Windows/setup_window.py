8473ea206a69dfc8fdcff4abed3c8eb4343a41fa
Aviv Bodeg <AVIVBodeg@gmail.com>
2024-08-14 18:39:04 +0300

    font and text size

--- Windows/setup_window.py
+++ Windows/setup_window.py
@@ -4,32 +4,34 @@ from Objects.vote_settings import VoteSettings
 
 
 def create_setup_window():
-    width, height = sg.Window.get_screen_size()
     layout_intro = [
-        [sg.Push(), sg.Text("Enter question: "), sg.Push()],
-        [sg.Push(), sg.InputText(size=(40, 1), k="-QUESTION-"), sg.Push()],
-        [sg.Push(), sg.InputText(size=(15, 1), k="-OPTION_1-", default_text="Option 1"),
-         sg.InputText(size=(15, 1), k="-OPTION_2-", default_text="Option 2"), sg.Push()],
+        [sg.Push(), sg.Text("Enter question: ", font=("Helvetica", 18)), sg.Push()],
+        [sg.Push(), sg.Multiline(k="-QUESTION-", font=("Helvetica", 18), size=(35, 3), no_scrollbar=True), sg.Push()],
+        [sg.Push(), sg.InputText(k="-OPTION_1-", default_text="Option 1", font=("Helvetica", 18), size=(15, 1)),
+         sg.InputText(k="-OPTION_2-", default_text="Option 2", font=("Helvetica", 18), size=(15, 1)), sg.Push()],
         [sg.VPush()],
         [sg.vbottom(sg.Text("Num. of voters: ", size=(13, 1))),
          sg.Slider((1, 100), 5, orientation="h", k="-NUM_VOTERS-")],
         [sg.VPush()],
-        [sg.Push(), sg.Button("OK"), sg.Push()],
-        [sg.Push(), sg.Checkbox("Auto mode?", k="-AUTO-")]
+        [sg.Push(), sg.Button("START", font=("Helvetica", 18), expand_x=True), sg.Push()],
+        # [sg.Push(), sg.Checkbox("Auto mode?", k="-AUTO-")]
     ]
 
-    window = sg.Window("Project", layout_intro, size=(int(width*0.5), int(height*0.5)))
+    window = sg.Window("Project", layout_intro)
 
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         window.close()
         return -1
-    elif event == "OK":
+    elif event == "START":
         question = values["-QUESTION-"] if values["-QUESTION-"] != "" else "Question?"
         num_voters = int(values["-NUM_VOTERS-"])
-        auto = values["-AUTO-"]
+        # auto = values["-AUTO-"]
         option1 = values["-OPTION_1-"] if values["-OPTION_1-"] != "" else "Option 1"
         option2 = values["-OPTION_2-"] if values["-OPTION_2-"] != "" else "Option 2"
 
         window.close()
         return VoteSettings(question, option1, option2, num_voters)
+
+
+create_setup_window()
--- Windows/vote_window.py
+++ Windows/vote_window.py
@@ -5,8 +5,12 @@ from Objects.question_info import QuestionInfo
 
 def create_vote_window(question_info: QuestionInfo, current_voter):
     layout_voting = [
-        [sg.Push(), sg.Text(question_info.question), sg.Push()],
-        [sg.Push(), sg.Button(question_info.opt1), sg.Button(question_info.opt2), sg.Push()]
+        [sg.Push(), sg.Text(question_info.question, font=("Helvetica", 20)), sg.Push()],
+        [sg.Push(), sg.Text(" ", font=("Helvetica", 20)), sg.Push()],
+        [sg.Push(), sg.Text(" ", font=("Helvetica", 20)), sg.Push()],
+        [sg.Push(), sg.Button(question_info.opt1, font=("Helvetica", 18)),
+         sg.Text(" ", font=("Helvetica", 18)),
+         sg.Button(question_info.opt2, font=("Helvetica", 18)), sg.Push()]
     ]
     window_voting = sg.Window("Voter #" + str(current_voter), layout_voting)
     event, values = window_voting.read()
