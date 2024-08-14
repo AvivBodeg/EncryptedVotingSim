737988723550bbd2e6de13ad4476065472d4686d
Aviv Bodeg <AVIVBodeg@gmail.com>
2024-08-14 19:15:11 +0300

    redisgn setup window

--- Windows/results_window.py
+++ Windows/results_window.py
@@ -5,8 +5,8 @@ from Objects.question_info import QuestionInfo
 def create_results_window(question_info: QuestionInfo, sum_opt1, sum_opt2):
     winner = question_info.opt1 if sum_opt1 >= sum_opt2 else question_info.opt2
     layout = [
-        [sg.Push(), sg.Text(question_info.question), sg.Push()],
-        [sg.Push(), sg.Text(winner + " was chosen"), sg.Push()]
+        [sg.Push(), sg.Text(question_info.question, font=("Helvetica", 18)), sg.Push()],
+        [sg.Push(), sg.Text(winner + " was chosen", font=("Helvetica", 18)), sg.Push()]
     ]
 
     window = sg.Window("Project", layout)
--- Windows/setup_window.py
+++ Windows/setup_window.py
@@ -7,31 +7,39 @@ def create_setup_window():
     layout_intro = [
         [sg.Push(), sg.Text("Enter question: ", font=("Helvetica", 18)), sg.Push()],
         [sg.Push(), sg.Multiline(k="-QUESTION-", font=("Helvetica", 18), size=(35, 3), no_scrollbar=True), sg.Push()],
-        [sg.Push(), sg.InputText(k="-OPTION_1-", default_text="Option 1", font=("Helvetica", 18), size=(15, 1)),
-         sg.InputText(k="-OPTION_2-", default_text="Option 2", font=("Helvetica", 18), size=(15, 1)), sg.Push()],
-        [sg.VPush()],
-        [sg.vbottom(sg.Text("Num. of voters: ", size=(13, 1))),
-         sg.Slider((1, 100), 5, orientation="h", k="-NUM_VOTERS-")],
-        [sg.VPush()],
-        [sg.Push(), sg.Button("START", font=("Helvetica", 18), expand_x=True), sg.Push()],
+        [sg.Text("Option 1:", font=("Helvetica", 18)),
+         sg.InputText(k="-OPTION_1-", font=("Helvetica", 18), size=(18, 1)), sg.Push()],
+        [sg.Text("Option 2:", font=("Helvetica", 18)),
+         sg.InputText(k="-OPTION_2-", font=("Helvetica", 18), size=(18, 1)), sg.Push()],
+        [sg.Text("Number of Voters: ", font=("Helvetica", 18)),
+         sg.Input(key='-NUM_VOTERS-', font=("Helvetica", 20), enable_events=True, size=(10, 1))],
+        [sg.Push(), sg.Button("START", font=("Helvetica", 18), expand_x=True), sg.Push()]
         # [sg.Push(), sg.Checkbox("Auto mode?", k="-AUTO-")]
     ]
 
     window = sg.Window("Project", layout_intro)
 
-    event, values = window.read()
-    if event == sg.WIN_CLOSED:
-        window.close()
-        return -1
-    elif event == "START":
-        question = values["-QUESTION-"] if values["-QUESTION-"] != "" else "Question?"
-        num_voters = int(values["-NUM_VOTERS-"])
-        # auto = values["-AUTO-"]
-        option1 = values["-OPTION_1-"] if values["-OPTION_1-"] != "" else "Option 1"
-        option2 = values["-OPTION_2-"] if values["-OPTION_2-"] != "" else "Option 2"
+    while True:
+        event, values = window.read()
 
-        window.close()
-        return VoteSettings(question, option1, option2, num_voters)
+        if event == sg.WIN_CLOSED:
+            window.close()
+            return -1
 
+        if event == '-NUM_VOTERS-':
+            input_value = values['-NUM_VOTERS-']
 
-create_setup_window()
+            # Allow only integer input: digits and a leading negative sign
+            if input_value and not (
+                    input_value.isdigit() or (input_value.startswith('-') and input_value[1:].isdigit())):
+                # If the input is not a valid integer, revert to the previous value
+                window['-NUM_VOTERS-'].update(input_value[:-1])
+
+        elif event == "START" and values['-NUM_VOTERS-'] != "":
+            question = values["-QUESTION-"] if values["-QUESTION-"] != "" else "Question?"
+            num_voters = int(values["-NUM_VOTERS-"])
+            option1 = values["-OPTION_1-"] if values["-OPTION_1-"] != "" else "Option 1"
+            option2 = values["-OPTION_2-"] if values["-OPTION_2-"] != "" else "Option 2"
+            # auto = values["-AUTO-"]
+            window.close()
+            return VoteSettings(question, option1, option2, num_voters)
